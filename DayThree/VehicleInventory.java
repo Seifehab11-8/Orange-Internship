25099ec5487626bebe5dae034e4d8f92fdb2e1bc
Seifehab11-8 <semaziz2004@yahoo.com>
2025-08-07 16:08:30 +0300

    fix for checkavailabilty location to inventory for single responsibility

--- DayThree/VehicleInventory.java
+++ DayThree/VehicleInventory.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 public class VehicleInventory {
     private List <Vehicle> inventoryList;
@@ -22,6 +23,41 @@ public class VehicleInventory {
     public Vehicle getVehicle(int index) {
         if(index < inventoryList.size()) {
             return inventoryList.get(index);
+        }
+        return null;
+    }
+
+    private boolean checkIfFeatureExist(Vehicle vehicle, VehicleFeature vehicleFeature) {
+        boolean isFound = false;
+        for(VehicleFeature vh : vehicle.getFeatures()) {
+            if(vh.equals(vehicleFeature)) {
+                isFound = true;
+                break;
+            }
+        }
+        return isFound;
+    }
+
+    public Vehicle checkAvailability(String type, Vector<VehicleFeature> desiredFeatures) {
+        boolean desiredCarFound = true;
+        for(int i = 0; i < inventoryList.size(); i++) {
+            desiredCarFound = true;
+            if(
+                    inventoryList.get(i).getVehicleType() == type 
+                && inventoryList.get(i).isAvailable() == true
+            ){
+                for(VehicleFeature vh : desiredFeatures) {
+                    if(!checkIfFeatureExist(inventoryList.get(i), vh)){
+                        desiredCarFound = false;
+                    }
+                }
+            }
+            else {
+                desiredCarFound = false;
+            }
+            if(desiredCarFound) {
+                return inventoryList.get(i);
+            }
         }
         return null;
     }
--- DayThree/VehicleOrderSystem.java
+++ DayThree/VehicleOrderSystem.java
@@ -6,43 +6,10 @@ public class VehicleOrderSystem {
         this.vehicleInventory = vehicleInventory;
     }
     
-    private boolean checkIfFeatureExist(Vehicle vehicle, VehicleFeature vehicleFeature) {
-        boolean isFound = false;
-        for(VehicleFeature vh : vehicle.getFeatures()) {
-            if(vh.equals(vehicleFeature)) {
-                isFound = true;
-                break;
-            }
-        }
-        return isFound;
-    }
 
-    private Vehicle checkAvailability(String type, Vector<VehicleFeature> desiredFeatures) {
-        boolean desiredCarFound = true;
-        for(int i = 0; i < vehicleInventory.getListSize(); i++) {
-            desiredCarFound = true;
-            if(
-                    vehicleInventory.getVehicle(i).getVehicleType() == type 
-                && vehicleInventory.getVehicle(i).isAvailable() == true
-            ){
-                for(VehicleFeature vh : desiredFeatures) {
-                    if(!checkIfFeatureExist(vehicleInventory.getVehicle(i), vh)){
-                        desiredCarFound = false;
-                    }
-                }
-            }
-            else {
-                desiredCarFound = false;
-            }
-            if(desiredCarFound) {
-                return vehicleInventory.getVehicle(i);
-            }
-        }
-        return null;
-    }
 
     public void placeOrder(String type, Vector<VehicleFeature> desiredFeatures) {
-        Vehicle vehicle = checkAvailability(type, desiredFeatures);
+        Vehicle vehicle = vehicleInventory.checkAvailability(type, desiredFeatures);
         if(vehicle != null) {
             vehicle.setAvailable(false);
             System.out.println("Vehicle reserved: " + vehicle.getRegistrationNumber());
